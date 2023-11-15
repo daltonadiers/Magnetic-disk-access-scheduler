7f8cdc794912fd9bed8744cac29fed9fed24aa99
dadiers <dalton-adiers@outlook.com>
2023-11-15 19:19:50 -0300

    Adicionando classes e Buffer para o print

---
+++ initializer.cpp
@@ -0,0 +1,13 @@
+/**
+ * @brief     Arquivo inicializador do projeto
+ * @details   Esse projeto foi desenvolvido para a cadeira de Sistemas Operacionais II, do curso de Ciência da Computação da Universidade de Passo Fundo.
+ * @authors   Dalton Oberdan Adiers - 193851
+ *            Bruna Baldissera - 195244
+ */
+#include "scheduler.cpp"
+
+int main(){
+    edm::scheduler s;
+    s.initializer();
+    return 0;
+}
---
+++ scheduler.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <random>
+#include <stdlib.h>
+#include <iomanip>
+#include <string>
+
+namespace edm{
+    class printer{
+        public:
+            void sumBuffer(std::string x){
+                ansPrint+=x;
+            }
+            void print(){
+                std::cout << ansPrint << "\n";
+            }
+        private:
+            std::string ansPrint="";
+    };
+    class scheduler{
+        public:
+            void initializer(){
+                geraRandom();
+                fcfs();
+                sstf();
+                scan();
+                cscan();
+                clook();
+                myPrinter.print();
+            }
+        private:
+            printer myPrinter;
+            int requisicoes[10];
+            std::set<int> posicoes;
+            int lastAcess =0, deslocamentos=0, posInicial, valueForDirection=0, backupforInitial;
+            void geraRandom(){
+                for(int i=0; i<11; i++){
+                    std::random_device rd;
+                    std::mt19937 gen(rd());
+                    std::uniform_int_distribution<int> distribution(1, 99);
+                    if(i<10) requisicoes[i] = distribution(gen);
+                    else posInicial = distribution(gen);
+                }
+                backupforInitial=posInicial;
+            }
+            void printOriginal(){
+                std::ostringstream output;
+                output << "\n" << "Posição inicial da cabeça de leitura e gravação " << std::setw(2) << std::setfill('0') << posInicial << "\n";
+                output << "Original [" << std::setw(2) << std::setfill('0') << requisicoes[0] << " - ";
+                for(int i=1; i<10; i++){
+                    output << std::setw(2) << std::setfill('0') << requisicoes[i];
+                    if(i!=9)output << " - ";
+                }
+                output << "]\n\n";
+                myPrinter.sumBuffer(output.str());
+            }
+            void print(){
+                std::ostringstream output;
+                int meuTamanho = 10-(int)posicoes.size();
+                int counter =0;
+                output << "Acessou " << std::setw(2) << std::setfill('0') << lastAcess << "[";
+                for(int i=0; i<10;i++){
+                    if(posicoes.count(i)==0){
+                        counter++;
+                        output << std::setw(2) << std::setfill('0') << requisicoes[i];
+                        if(counter==meuTamanho){
+                            continue;
+                        }else{
+                            output << " - ";
+                        }
+                    }
+                }
+                output << "] Já deslocou " << deslocamentos << "\n";
+                myPrinter.sumBuffer(output.str());
+            }
+
+            void zeroAll(){
+                posInicial=backupforInitial,lastAcess=posInicial,deslocamentos=0;posicoes.clear();valueForDirection=posInicial;
+            }
+
+            int foundMinValue(){
+                int minValue =100;
+                for(int i=0; i<10; i++){
+                    if(requisicoes[i]<minValue && posicoes.count(i)==0)minValue=requisicoes[i];
+                }
+                return minValue;
+            }
+
+            void goUp(){
+                    for(int i=0; i<10; i++){
+                        bool mudei = false;
+                        int minAbsFound =200, indexOfAbs=0;
+                        for(int j=0; j<10; j++){
+                            if(posicoes.count(j)==0 && requisicoes[j]>=valueForDirection){
+                                if(abs(posInicial-requisicoes[j])<minAbsFound){
+                                    minAbsFound=abs(posInicial-requisicoes[j]);
+                                    indexOfAbs=j; mudei=true;
+                                }
+                            }
+                        }
+                        if(mudei){
+                            lastAcess = requisicoes[indexOfAbs]; posicoes.insert(indexOfAbs);deslocamentos+=minAbsFound;posInicial=requisicoes[indexOfAbs];
+                            print();
+                        }
+                    }
+            }
+            void goDown(){
+                    for(int i=0; i<10; i++){
+                        int minAbsFound =200, indexOfAbs=0;
+                        bool mudei = false;
+                        for(int j=0; j<10; j++){
+                            if(posicoes.count(j)==0 && requisicoes[j]<=valueForDirection){
+                                if(abs(posInicial-requisicoes[j])<minAbsFound){
+                                    minAbsFound=abs(posInicial-requisicoes[j]);
+                                    indexOfAbs=j; mudei=true;
+                                }
+                            }
+                        }
+                        if(mudei){
+                            lastAcess = requisicoes[indexOfAbs]; posicoes.insert(indexOfAbs);deslocamentos+=minAbsFound;posInicial=requisicoes[indexOfAbs];
+                            print();
+                        }
+                    }
+            }
+
+            void fcfs(){
+                printOriginal();
+                std::ostringstream output;
+                myPrinter.sumBuffer("========== Algoritmo FCFS! ==========\n");
+                for(int i=0; i<10; i++){
+                    lastAcess=requisicoes[i];
+                    posicoes.insert(i);
+                    deslocamentos += abs(posInicial-lastAcess);
+                    posInicial=lastAcess;
+                    print();
+                }
+                output << "FCFS - Quantidade total de deslocamentos: " << deslocamentos << "\n";
+                myPrinter.sumBuffer(output.str());
+            }
+            void sstf(){
+                std::ostringstream output;
+                zeroAll();
+                printOriginal();
+                myPrinter.sumBuffer("========== Algoritmo SSTF! ==========\n");
+                for(int i=0; i<10;i++){
+                    int minAbsFound =200, indexOfAbs=0;
+                    for(int j=0; j<10; j++){
+                        if(posicoes.count(j)==0){
+                            if(abs(posInicial-requisicoes[j])<minAbsFound){
+                                minAbsFound=abs(posInicial-requisicoes[j]);
+                                indexOfAbs=j;
+                            }
+                        }
+                    }
+                    lastAcess = requisicoes[indexOfAbs]; posicoes.insert(indexOfAbs);deslocamentos+=minAbsFound;posInicial=requisicoes[indexOfAbs];
+                    print();
+                }
+                output << "SSTF - Quantidade total de deslocamentos: " << deslocamentos << "\n";
+                myPrinter.sumBuffer(output.str());
+            }
+
+            void scan(){
+                std::ostringstream output;
+                zeroAll();
+                printOriginal();
+                myPrinter.sumBuffer("===== Algoritmo Scan (elevador) =====\n");
+                goDown();
+                if(posicoes.size()!=10){
+                    if(lastAcess!=0){
+                        deslocamentos+=abs(lastAcess-0);lastAcess=0;posInicial=0;
+                        print();
+                    }
+                    valueForDirection=0;
+                    goUp();
+                }
+                output << "SCAN - Quantidade total de deslocamentos: " << deslocamentos << "\n";
+                myPrinter.sumBuffer(output.str());
+            }
+
+            void cscan(){
+                std::ostringstream output;
+                zeroAll();
+                printOriginal();
+                myPrinter.sumBuffer("====== Algoritmo Circular SCAN ======\n");
+                goUp();
+                if(posicoes.size()!=10){
+                    if(lastAcess!=99){
+                        deslocamentos+=abs(lastAcess-99);lastAcess=99;posInicial=99;
+                        print();
+                    }
+                    deslocamentos+=abs(lastAcess-0);lastAcess=0;posInicial=0;
+                    print();
+                    valueForDirection=0;
+                    goUp();
+                }
+                output << "Circular Scan - Quantidade total de deslocamentos: " << deslocamentos << "\n";
+                myPrinter.sumBuffer(output.str());
+            }
+            void clook(){
+                std::ostringstream output;
+                zeroAll();
+                printOriginal();
+                myPrinter.sumBuffer("========= Algoritmo C-look ==========\n");
+                goUp();
+                if(posicoes.size()!=10){
+                    int b = foundMinValue();
+                    deslocamentos+=abs(lastAcess-b);lastAcess=0;posInicial=b;posicoes.insert(b);
+                    valueForDirection=0;
+                    goUp();
+                }
+                output << "C-look - Quantidade total de deslocamentos: " << deslocamentos << "\n";
+                myPrinter.sumBuffer(output.str());
+            }
+    };
+}
